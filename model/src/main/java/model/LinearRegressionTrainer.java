58b438b4ef71bb60fe4ba8319d01ac23f57a3092
lucad <l.depasquale5@studenti.unisa.it>
2025-05-08 20:20:23 +0200

    Fix for spark session

--- model/src/main/java/model/LinearRegressionTrainer.java
+++ model/src/main/java/model/LinearRegressionTrainer.java
@@ -90,10 +90,11 @@ public class LinearRegressionTrainer {
         System.out.println("R2: " + r2);
         System.out.println(model.coefficients());
 
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+        /*Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             session.stop();
             session.close();
-        }));
+
+        }));*/
 
         //model.save("C:/Users/lucad/Desktop/models/model");
         //scalerModel.save("C:/Users/lucad/Desktop/models/scaler");
